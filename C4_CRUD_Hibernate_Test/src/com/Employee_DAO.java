ea9f7f054f8d94486abe08f8d14923e295e48ba0
Swapnil Bamble <swapnilbamble4@gmail.com>
2023-11-08 00:20:35 +0530

    Update Employee_DAO.java

--- C4_CRUD_Hibernate_Test/src/com/Employee_DAO.java
+++ C4_CRUD_Hibernate_Test/src/com/Employee_DAO.java
@@ -2,7 +2,6 @@ package com;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 import com.Employee;
